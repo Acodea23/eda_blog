5600c4694d404dad587a0005d480c84e27917f69
Matthew Wilson <wilson.matthewg@gmail.com>
2025-11-12 13:37:41 -0700

    hyping corbin up

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 #uv run streamlit run app.py
 
-st.title("Covid Death Demographics EDA")
-
+#st.title("Covid Death Demographics EDA")
+st.title("Corbin is a cool guy")
 df = pd.read_csv("covid_death_demographics.csv")
 st.write(df)
 
