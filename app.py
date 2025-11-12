a939806b2a251415dfcdf0d483ce36a5358979b0
Matthew Wilson <wilson.matthewg@gmail.com>
2025-11-12 16:01:39 -0700

    finished with code for scraping and eda

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import pandas as pd
 
 #uv run streamlit run app.py
-
-#st.title("Covid Death Demographics EDA")
-st.title("Corbin is a cool guy")
 df = pd.read_csv("covid_death_demographics.csv")
+df_st = st.dataframe(df)
+
+
+st.title("Covid Death Demographics EDA")
+st.subheader("Determining susceptability of young people to covid by demographic groups")
+
 st.write(df)
+
 
 
 
